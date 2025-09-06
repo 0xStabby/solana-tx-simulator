8849f7a5bb522e5d7add327d049ff25fe43c4f36
0xstabby <0xstabby@protonmail.com>
2025-09-06 15:31:23 -0700

    set initial version, some extra exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { simulateTransaction, simulateEncodedTransaction } from "./utils/simulateTransaction";
+import { BalanceChangeResult, SuccessResult, FailureResult, BalanceChange } from "./types/balanceChange";
 
-export { simulateTransaction, simulateEncodedTransaction };
+export {
+  simulateTransaction,
+  simulateEncodedTransaction,
+  BalanceChangeResult,
+  SuccessResult,
+  FailureResult,
+  BalanceChange,
+};
